7a83a81443feba54e7761455948523a4c5c62b20
macurrent <bruceminanga@gmail.com>
2022-07-15 16:13:20 +0300

    My first commit

---
+++ courses/templatetags/course.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def model_name(obj):
+    try:
+        return obj._meta.model_name
+    except AttributeError:
+        return None
---
+++ students/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django import forms
+from courses.models import Course
+
+
+class CourseEnrollForm(forms.Form):
+    course = forms.ModelChoiceField(queryset=Course.objects.all(),
+                                    widget=forms.HiddenInput)
